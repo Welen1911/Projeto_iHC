51e834c0c3fcd1ca7c0276f9be03a03cd8f9f3b4
Welen1911 <welenalmeida2210@gmail.com>
2023-07-06 14:15:23 -0300

    pagina principal "pronta"

--- js/scripts.js
+++ js/scripts.js
@@ -45,18 +45,7 @@
     .on('mouseenter mouseleave','.dropdown',toggleDropdown)
     .on('click', '.dropdown-menu a', toggleDropdown);
 
-
-    /* Move Form Fields Label When User Types */
-    // for input and textarea fields
-    $("input, textarea").keyup(function(){
-		if ($(this).val() != '') {
-			$(this).addClass('notEmpty');
-		} else {
-			$(this).removeClass('notEmpty');
-		}
-	});
 	
-
     /* Back To Top Button */
     // create the back to top button
     $('body').prepend('<a href="body" class="back-to-top page-scroll">Back to Top</a>');
